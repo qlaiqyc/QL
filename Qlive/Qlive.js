fbc2b67ade27262dc8f70518ef05f340c2c80b2a
qlaiqyc <qyc123abc>
2017-03-29 18:00:39 +0800

    设计

---
+++ Qlive/Qlive.js
@@ -0,0 +1,40 @@
+(function(window){
+	
+	/* 
+	 * 1.单个对象注册
+	 * 2.监听对象 生命周期（根据路由进行判断）
+	 * 
+	 * 
+	 * 
+	 * 
+	 * */
+	
+	var FunUtil = {};
+	var PageObj = {};
+	
+	FunUtil.Global = {
+		"pages":[]
+	};
+	
+	FunUtil.common4pageInfo = function(data){
+		
+		var execuFun = {};
+		
+		
+	};
+	
+	
+	FunUtil.register = function(data){
+		
+		
+		
+		
+	};
+	
+	
+	
+	
+	
+	
+	
+})(window);
